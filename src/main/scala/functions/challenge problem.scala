320eda86ce0581e7b465ea23af3caff2d32b8cb0
coal05 <ccrace24@simpsonacademy.net>
2022-06-09 15:31:51 +0000

    day 4

--- src/main/scala/functions/challenge problem.scala
+++ src/main/scala/functions/challenge problem.scala
@@ -1,6 +1,17 @@
 package functions
-object challengeproblem{
 
-def pythagoreantriples(a: Int, b: Int, c: Int) : Int={}
+object challengeproblem {
 
+  def triples(n: Int): List[List[Int]] = {
+    for {
+      a <- (1 to n).toList
+      b <- (1 to n).toList
+      c <- (1 to n).toList
+      if (a * a + b * b == c * c)
+    } yield List(a, b, c)
+  }
 
+  def main(argv: Array[String]): Unit = {
+    println(triples(100))
+  }
+}
